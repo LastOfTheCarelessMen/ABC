c08cd27dcc22f76d2fd858d81c58991f0b905e00
SF <last.of.the.careless.men@gmail.com>
2009-12-16 09:34:37 -0500

    Move the grammar to the ABC.pm file.

--- lib/ABC.pm
+++ lib/ABC.pm
@@ -1,5 +1,22 @@
 use v6;
 
+grammar ABC
+{
+    regex header_field_name { \w }
+    regex header_field_data { \N* }
+    regex header_field { ^^ <header_field_name> ':' \s* <header_field_data> $$ }
+    regex header { [<header_field> \n]+ }
+
+    regex basenote { <[a..g]+[A..G]> }
+    regex octave { \'+ | \,+ }
+    regex accidental { '^' | '^^' | '_' | '__' | '=' }
+    regex pitch { <accidental>? <basenote> <octave>? }
+
+    regex tie { '-' }
+    regex note_length { [\d* ['/' \d*] ] | '/' }
+    regex note { <pitch> <note_length>? <tie>? }
+}
+
 class ABCHeader
 {
     
--- playing.pl
+++ playing.pl
@@ -1,5 +1,8 @@
 use v6;
 
+BEGIN { push @*INC, "lib" }
+use ABC;
+
 my $abc = q«X:64
 T:Cuckold Come Out o' the Amrey
 S:Northumbrian Minstrelsy
@@ -10,23 +13,6 @@ A/B/c/A/ +trill+c>d e>deg | GG +trill+B>c d/B/A/G/ B/c/d/B/ |
 A/B/c/A/ c>d e>deg | dB/A/ gB +trill+A2 +trill+e2 ::
 g>ecg ec e/f/g/e/ | d/c/B/A/ Gd BG B/c/d/B/ | 
 g/f/e/d/ c/d/e/f/ gc e/f/g/e/ | dB/A/ gB +trill+A2 +trill+e2 :|»;
-
-grammar ABC
-{
-    regex header_field_name { \w }
-    regex header_field_data { \N* }
-    regex header_field { ^^ <header_field_name> ':' \s* <header_field_data> $$ }
-    regex header { [<header_field> \n]+ }
-
-    regex basenote { <[a..g]+[A..G]> }
-    regex octave { \'+ | \,+ }
-    regex accidental { '^' | '^^' | '_' | '__' | '=' }
-    regex pitch { <accidental>? <basenote> <octave>? }
-
-    regex tie { '-' }
-    regex note_length { [\d* ['/' \d*] ] | '/' }
-    regex note { <pitch> <note_length>? <tie>? }
-}
 
 if $abc ~~ m/ <ABC::header> /
 {
