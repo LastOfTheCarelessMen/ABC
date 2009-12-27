0aaec06ba32547f66e3a5280af0977f87252fcf9
SF <last.of.the.careless.men@gmail.com>
2009-12-26 20:23:32 -0500

    playing.pl now has test code to go through a matched tune and pull out all the notes.

--- playing.pl
+++ playing.pl
@@ -14,15 +14,20 @@ A/B/c/A/ c>d e>deg | dB/A/ gB +trill+A2 +trill+e2 ::
 g>ecg ec e/f/g/e/ | d/c/B/A/ Gd BG B/c/d/B/ | 
 g/f/e/d/ c/d/e/f/ gc e/f/g/e/ | dB/A/ gB +trill+A2 +trill+e2 :|»;
 
-if $abc ~~ m/ <ABC::header> /
+my $match = $abc ~~ m/ <ABC::tune> /;
+
+die "Tune not matched\n" unless $match ~~ Match;
+
+my @notes = gather for $match<ABC::tune><music><line_of_music> -> $line
 {
-    for $<ABC::header><header_field> -> $line
+    for $line<bar> -> $bar
     {
-        say "header: {$line<header_field_name>}: {$line<header_field_data>}";
+        for $bar<element>
+        {
+            when .<broken_rhythm> { take .<broken_rhythm><note>[0]; take .<broken_rhythm><note>[1]; }
+            when .<note>          { take .<note>; }
+        }
     }
 }
 
-if "^^A/3" ~~ m/ <ABC::note> /
-{
-    say $<ABC::note>.perl;
-}
+@notes.map({.say});
