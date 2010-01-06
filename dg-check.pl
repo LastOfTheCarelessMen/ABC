ffafa4bc817a7cefb5418d47dfd81c238d8fd606
SF <last.of.the.careless.men@gmail.com>
2010-01-06 13:23:10 -0500

    Simplify input, work old note handling code into new match loop.

--- dg-check.pl
+++ dg-check.pl
@@ -3,31 +3,26 @@ use v6;
 BEGIN { push @*INC, "lib" }
 use ABC;
 
-regex between { \v+ }
-regex file { \s* <ABC::tune> ** <between> }
-
-my $abcs = $*IN.lines.join("\n");
-my @matches = $abcs.comb(m/ <ABC::tune> /, :match);
+my @matches = $*IN.lines.join("\n").comb(m/ <ABC::tune> /, :match);
 
 for @matches {
     my %header = header_hash(.<ABC::tune><header>);
-    say %header<T>;
-}
+    say %header<T> ~ ":";
 
-# 
-# my @notes = gather for $match<ABC::tune><music><line_of_music> -> $line
-# {
-#     for $line<bar> -> $bar
-#     {
-#         for $bar<element>
-#         {
-#             when .<broken_rhythm> { take .<broken_rhythm><note>[0]; take .<broken_rhythm><note>[1]; }
-#             when .<note>          { take .<note>; }
-#         }
-#     }
-# }
-# 
-# my %header = header_hash($match<ABC::tune><header>);
-# my %key_signature = key_signature(%header<K>);
-# 
-# @notes.map({say .<pitch> ~ " => " ~ apply_key_signature(%key_signature, .<pitch>)});
+    my @notes = gather for .<ABC::tune><music><line_of_music> -> $line
+    {
+        for $line<bar> -> $bar
+        {
+            for $bar<element>
+            {
+                when .<broken_rhythm> { take .<broken_rhythm><note>[0]; take .<broken_rhythm><note>[1]; }
+                when .<note>          { take .<note>; }
+            }
+        }
+    }
+
+    my %header = header_hash(.<ABC::tune><header>);
+    my %key_signature = key_signature(%header<K>);
+
+    @notes.map({say .<pitch> ~ " => " ~ apply_key_signature(%key_signature, .<pitch>)});
+}
