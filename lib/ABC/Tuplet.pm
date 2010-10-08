1e84ef1d3def2509fd50f855fdc1f81bfff2fba8
Solomon Foster <colomon@gmail.com>
2010-10-08 04:01:41 -0400

    Start ABC::Tuplet class.

---
+++ lib/ABC/Tuplet.pm
@@ -0,0 +1,14 @@
+use v6;
+
+use ABC::Duration;
+
+class ABC::Tuplet does ABC::Duration {
+    has $.tuple;
+    has @.notes;
+    
+    method new($tuple, @notes) {
+        fail "Stem must have at least one note" if +@notes == 0;
+        fail "Only handle triplets so far" if $.tuple != 3;
+        self.bless(*, :$tuple, :@notes, :ticks(2/3 * [+] @notes>>.ticks));
+    }
+}
